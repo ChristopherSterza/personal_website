6aa00097b5adb3200e674331f682ca2d5a16b0d4
Christopher <csterza@ucsc.edu>
2022-01-28 16:33:18 -0800

    scaled images to make the page load faster

--- birthday/script.js
+++ birthday/script.js
@@ -14,19 +14,19 @@ myButton.onclick = function() {
     if (numCover > 5) numCover = 1;
     switch (numCover){
         case 1:
-            url = 'images/romantic.jpg';
+            url = 'images/romantic.png';
             break;
         case 2:
             url = 'images/smokey.png';
             break;
         case 3:
-            url = 'images/covid.jpg';
+            url = 'images/covid.png';
             break;
         case 4:
-            url = 'images/injury.jpg';
+            url = 'images/injury.png';
             break;
         case 5:
-            url = 'images/masks.jpg';
+            url = 'images/masks.png';
             break;
     }
     console.log(url);
