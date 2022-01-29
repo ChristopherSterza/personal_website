fbeb935c74d920a986b65be15e07977fb73989b4
Christopher <csterza@ucsc.edu>
2022-01-28 18:16:45 -0800

    Tried to center heart with js

--- birthday/script.js
+++ birthday/script.js
@@ -4,11 +4,18 @@ myButton = document.querySelector("#heart");
 
 var numCover = 1;
 
-myCard.onclick = function() {
-    mycb.checked = !mycb.checked;
+myCard.onclick = function(e) {
+    if (e.target != myButton){
+        if (mycb.checked){
+            myButton.style.display = "block";
+        } else {
+            myButton.style.display = "none";
+        }
+        mycb.checked = !mycb.checked;
+    }
 }
 
-myButton.onclick = function() {
+myButton.onclick = function(e) {
     var url = '';
     numCover++;
     if (numCover > 5) numCover = 1;
